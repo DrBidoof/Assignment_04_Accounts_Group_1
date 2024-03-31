5332a96aede333cab31d924c2dedcdc08501cb4e
DrBidoof <alwyn.lynch23@gmail.com>
2024-03-31 19:55:29 -0400

    All classes for project

---
+++ Assignment_04_Accounts_Group_1/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_04_Accounts_Group_1
+{
+    public abstract class Account
+    {
+
+    }
+}
